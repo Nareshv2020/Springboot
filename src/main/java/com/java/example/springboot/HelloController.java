0677a3e05fe0212d0ba3222021546113a8e3c652
Nareshv2020 <70183492+Nareshv2020@users.noreply.github.com>
2020-08-24 20:49:38 -0500

    Test
    Test

---
+++ src/main/java/com/java/example/springboot/HelloController.java
@@ -0,0 +1,15 @@
+package com.java.example.springboot;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/hello")
+@RestController
+public class HelloController {
+
+    @GetMapping
+    public String hello() {
+        return "Welomce wot AWS";
+    }
+}
